f5027a1d9cd9bf5e5ca8f8e35a3a69eb3763d1dd
Sumit Singh <sumitsingh@Nagarros-MacBook-Pro.local>
2021-08-13 20:05:08 +0530

    findByEventName

--- src/main/java/com/springdatarest/event_management/repositories/EventRepositoryInterface.java
+++ src/main/java/com/springdatarest/event_management/repositories/EventRepositoryInterface.java
@@ -3,6 +3,11 @@ package com.springdatarest.event_management.repositories;
 import com.springdatarest.event_management.entities.Event;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface EventRepositoryInterface extends PagingAndSortingRepository<Event, Long> {
+
+    List<Event> findByName(@Param("name") String eventName); // pass req param as name:value
 }
