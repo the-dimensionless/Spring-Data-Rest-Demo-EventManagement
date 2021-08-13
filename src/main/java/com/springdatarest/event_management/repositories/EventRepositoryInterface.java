981326c3a788d20ac17fcd6aa4b8eb819fdd4715
Sumit Singh <sumitsingh@Nagarros-MacBook-Pro.local>
2021-08-13 20:10:59 +0530

    paging with findBy

--- src/main/java/com/springdatarest/event_management/repositories/EventRepositoryInterface.java
+++ src/main/java/com/springdatarest/event_management/repositories/EventRepositoryInterface.java
@@ -1,6 +1,8 @@
 package com.springdatarest.event_management.repositories;
 
 import com.springdatarest.event_management.entities.Event;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
@@ -9,5 +11,5 @@ import java.util.List;
 
 public interface EventRepositoryInterface extends PagingAndSortingRepository<Event, Long> {
 
-    List<Event> findByName(@Param("name") String eventName); // pass req param as name:value
+    Page<Event> findByName(@Param("name") String eventName, Pageable pageable); // pass req param as name:value
 }
